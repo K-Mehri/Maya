aa933ebf97e2800563bf3cc052b149d7dc588e2b
mjrstl <javadrostamlou930@gmail.com>
2026-09-18 17:18:25 +0330

    frontend base project with tanstack, TS, vite, tailwindcss #2

---
+++ maya-frontend/src/routes/users/index.tsx
@@ -0,0 +1,11 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/users/")({
+    component: RouteComponent,
+});
+
+function RouteComponent() {
+    return <div className=''>
+        users main page
+    </div>;
+}
